2c7830ad1296e39b31dac0e7b5be26c7c9c0397e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-01-21 22:47:13 -0500

    mocha

--- example.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-var zipwith = require("./zipwith");
-
-console.log(zipwith.zipwith(function(a, b) { return a + b; }, [1, 2, 3], [3, 2, 1]));
-console.log(zipwith.unzip([[1, 2], [3, 4], [5, 6]]));
