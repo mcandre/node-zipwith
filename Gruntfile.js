5a2a22f1169f174f14c41fe73203d476b2c85801
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 19:07:26 -0600

    update editorconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,31 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    exec: {
-      test: 'npm test',
+    grunt.initConfig({
+        exec: {
+            test: 'npm test',
+            jshint: 'node node_modules/jshint/bin/jshint .',
+            jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
+            eslint: 'node node_modules/eslint/bin/eslint .',
+            aspelllint: 'aspelllint .',
+            editorconfig: 'git ls-files -z | grep -av patch | xargs -0 -r -n 100 eclint check'
+        }
+    });
 
-      jshint: 'node node_modules/jshint/bin/jshint .',
-      jslint: 'find . -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type f -name "*.bat" -prune -o -type f \\( -wholename "*/bin/*" -or -name "*.js" \\) -exec node_modules/.bin/jslint {} \\;',
-      eslint: 'node node_modules/eslint/bin/eslint .',
-      lili: 'bundle exec lili .',
-      aspelllint: 'aspelllint .',
-      editorconfig: 'flcl . | xargs -n 100 editorconfig-cli check'
-    }
-  });
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', ['exec:test']);
+    grunt.registerTask('test', ['exec:test']);
+    grunt.registerTask('lint', [
+        'exec:jshint',
+        // Go to bed, Crockford
+        // 'exec:jslint',
+        'exec:eslint',
+        'exec:editorconfig'
+    ]);
 
-  grunt.loadNpmTasks('grunt-exec');
-
-  grunt.registerTask('default', ['exec:test']);
-  grunt.registerTask('test', ['exec:test']);
-
-  grunt.registerTask('lint', [
-    'exec:jshint',
-    // Go to bed, Crockford
-    // 'exec:jslint',
-    'exec:eslint',
-    'exec:lili',
-    'exec:editorconfig'
-  ]);
-
-  grunt.registerTask('jshint', ['exec:jshint']);
-  grunt.registerTask('jslint', ['exec:jslint']);
-  grunt.registerTask('eslint', ['exec:eslint']);
-  grunt.registerTask('lili', ['exec:lili']);
-  grunt.registerTask('aspelllint', ['exec:aspelllint']);
-  grunt.registerTask('editorconfig', ['exec:editorconfig']);
+    grunt.registerTask('jshint', ['exec:jshint']);
+    grunt.registerTask('jslint', ['exec:jslint']);
+    grunt.registerTask('eslint', ['exec:eslint']);
+    grunt.registerTask('aspelllint', ['exec:aspelllint']);
+    grunt.registerTask('editorconfig', ['exec:editorconfig']);
 };
--- test/test.js
+++ test/test.js
@@ -7,47 +7,47 @@ cmp = require("cmp"),
 zipwith = require("../lib/zipwith");
 
 describe("zipwith", function() {
-  describe("should be able to zip", function () {
-    it("should zip", function() {
-      assert.equal(
-        cmp.eq(
-          zipwith.zipwith(function (a, b) { return [a, b]; }, [1, 2, 3], [4, 5, 6]),
-          [[1, 4], [2, 5], [3, 6]]
-        ),
-        true
-      );
+    describe("should be able to zip", function () {
+        it("should zip", function() {
+            assert.equal(
+                cmp.eq(
+                    zipwith.zipwith(function (a, b) { return [a, b]; }, [1, 2, 3], [4, 5, 6]),
+                    [[1, 4], [2, 5], [3, 6]]
+                ),
+                true
+            );
+        });
     });
-  });
 
-  describe("should be able to unzip", function() {
-    it("should unzip", function() {
-      assert.equal(
-        cmp.eq(
-          zipwith.unzip([[1, 4], [2, 5], [3, 6]]),
-          [1, 2, 3, 4, 5, 6]
-        ),
-        true
-      );
+    describe("should be able to unzip", function() {
+        it("should unzip", function() {
+            assert.equal(
+                cmp.eq(
+                    zipwith.unzip([[1, 4], [2, 5], [3, 6]]),
+                    [1, 2, 3, 4, 5, 6]
+                ),
+                true
+            );
+        });
     });
-  });
 
-  describe("reversible", function() {
-    it("should be reversible", function() {
-      function propReversible(xs, ys) {
-        if (xs.length === ys.length) {
-          return cmp.eq(
-            zipwith.unzip(zipwith.zipwith(function (a, b) { return [a, b]; }, xs, ys)),
-            xs.concat(ys)
-          );
-        }
-        else {
-          return true;
-        }
-      }
+    describe("reversible", function() {
+        it("should be reversible", function() {
+            function propReversible(xs, ys) {
+                if (xs.length === ys.length) {
+                    return cmp.eq(
+                        zipwith.unzip(zipwith.zipwith(function (a, b) { return [a, b]; }, xs, ys)),
+                        xs.concat(ys)
+                    );
+                }
+                else {
+                    return true;
+                }
+            }
 
-      function arbIntArray() { return qc.arbArray(qc.arbByte); }
+            function arbIntArray() { return qc.arbArray(qc.arbByte); }
 
-      assert.equal(true, qc.forAll(propReversible, arbIntArray, arbIntArray));
+            assert.equal(true, qc.forAll(propReversible, arbIntArray, arbIntArray));
+        });
     });
-  });
 });
